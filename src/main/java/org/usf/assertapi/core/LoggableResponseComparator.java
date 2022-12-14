a0be13504c1914e76c112060af5fb3b1d89a88a3
u$f <usf.alami@gmail.com>
2022-12-14 12:28:08 +0100

    edit

--- src/main/java/org/usf/assertapi/core/LoggableResponseComparator.java
+++ src/main/java/org/usf/assertapi/core/LoggableResponseComparator.java
@@ -14,6 +14,7 @@ public final class LoggableResponseComparator implements ResponseComparator {
 
 	private final ResponseComparator comparator;
 
+	@Override
 	public void assumeEnabled(ApiRequest query) {
 		if(query.getConfiguration().isEnable()) {
 			log.info("API test run {}", query);
@@ -24,45 +25,54 @@ public final class LoggableResponseComparator implements ResponseComparator {
 		comparator.assumeEnabled(query);
 	}
 
+	@Override
 	public void assertionFail(Throwable t) {
 		log.error("API test fail", t);
 		comparator.assertionFail(t);
 	}
 
+	@Override
 	public void assertStatusCode(int expectedStatusCode, int actualStatusCode) {
 		logComparaison("statusCode", expectedStatusCode, actualStatusCode);
 		comparator.assertStatusCode(expectedStatusCode, actualStatusCode);
 	}
 
+	@Override
 	public void assertContentType(MediaType expectedContentType, MediaType actualContentType) {
 		logComparaison("mediaType", expectedContentType, actualContentType);
 		comparator.assertContentType(expectedContentType, actualContentType);
 	}
 
+	@Override
 	public void assertByteContent(byte[] expectedContent, byte[] actualContent) {
 		logComparaison("byteContent", expectedContent, actualContent); //just reference
 		comparator.assertByteContent(expectedContent, actualContent);
 	}
 
+	@Override
 	public void assertTextContent(String expectedContent, String actualContent) {
 		logComparaison("textContent", expectedContent, actualContent);
 		comparator.assertTextContent(expectedContent, actualContent);
 	}
 
+	@Override
 	public void assertJsonContent(String expectedContent, String actualContent, boolean strict) {
 		logComparaison("jsonContent" + (strict ? "(strict)" : ""), expectedContent, actualContent);
 		comparator.assertJsonContent(expectedContent, actualContent, strict);
 	}
 
+	@Override
 	public void assertJsonCompareResut(JSONCompareResult res) {
 		//should not be call
 	}
 
+	@Override
 	public void assertOK() {
 		log.info("API test OK");
 		comparator.assertOK();
 	}
 
+	@Override
 	public <T> T execute(boolean expected, Supplier<T> c) {
 		return comparator.execute(expected, c);
 	}

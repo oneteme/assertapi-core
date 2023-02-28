cec2da6bc4f5a38dfc583f25390448f1b8f16863
u$f <usf.alami@gmail.com>
2023-02-28 10:07:00 +0100

    edit

--- src/test/java/org/usf/assertapi/core/TemporalShiftTest.java
+++ src/test/java/org/usf/assertapi/core/TemporalShiftTest.java
@@ -1,33 +1,34 @@
 package org.usf.assertapi.core;
 
-import static java.time.temporal.ChronoUnit.DAYS;
-import static java.time.temporal.ChronoUnit.HOURS;
-import static java.time.temporal.ChronoUnit.MILLIS;
-import static java.time.temporal.ChronoUnit.MINUTES;
-import static java.time.temporal.ChronoUnit.MONTHS;
-import static java.time.temporal.ChronoUnit.NANOS;
-import static java.time.temporal.ChronoUnit.SECONDS;
-import static java.time.temporal.ChronoUnit.WEEKS;
-import static java.time.temporal.ChronoUnit.YEARS;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.usf.assertapi.core.TemporalShift.parseUnit;
 import static org.usf.junit.addons.AssertExt.assertThrowsWithMessage;
 
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TemporalShiftTest {
-
+	
+	@ParameterizedTest
+	@CsvSource({
+			"y,YEARS", 
+			"m,MONTHS", 
+			"w,WEEKS", 
+			"d,DAYS", 
+			"h,HOURS", 
+			"min,MINUTES", 
+			"s,SECONDS", 
+			"ms,MILLIS", 
+			"ns,NANOS"})
+	void testParseUnit(String arg, ChronoUnit expected) {
+		assertSame(expected, parseUnit(arg));
+	}
+	
 	@Test
-	void testParseUnit() {
-		assertSame(YEARS, parseUnit("y"));
-		assertSame(MONTHS, parseUnit("m"));
-		assertSame(WEEKS, parseUnit("w"));
-		assertSame(DAYS, parseUnit("d"));
-		assertSame(HOURS, parseUnit("h"));
-		assertSame(MINUTES, parseUnit("min"));
-		assertSame(SECONDS, parseUnit("s"));
-		assertSame(MILLIS, parseUnit("ms"));
-		assertSame(NANOS, parseUnit("ns"));
+	void testParseUnit_unsupported() {
 		assertThrowsWithMessage(UnsupportedOperationException.class, "unsupported unit : a", 
 				()-> parseUnit("a"));
 	}
